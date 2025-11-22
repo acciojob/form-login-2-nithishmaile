57bc1d16e7283421d7af2026049476a404bab316
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-11-22 12:06:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const form=document.getElementById("form");
 
-form.addEventListener("submit",()=>{
+form.addEventListener("submit",(e)=>{
 	e.preventDefault()
 	alert(`First Name: ${e.target[0].value} Last Name: ${e.target[1].value} Phone Number: ${e.target[2].value} Email ID: ${e.target[3].value}`)
 })
